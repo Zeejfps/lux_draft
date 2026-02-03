8fe7c434191b680b04fc767104f7b4171f49c709
Zee Vasilyev <zeejfps@gmail.com>
2026-02-03 09:03:50 -0600

    Trying to fix viteconfig so it can load resources

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/lux_draft/',
+  base: './',
   test: {
     environment: 'jsdom',
     globals: true,
