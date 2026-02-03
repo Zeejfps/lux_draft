5e18611e696ebd93e063cf92eac84e14130cfddf
Zee Vasilyev <zeejfps@gmail.com>
2026-02-03 09:08:06 -0600

    stil trying to fix the path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: './',
+  base: './lux_draft/',
   test: {
     environment: 'jsdom',
     globals: true,
